311a3d65faf2133041facf7b2775b43c450e2dac
Maite Gracia <maitegracia223@gmail.com>
2017-05-16 14:24:08 +0100

    Skills section animation done

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -89,31 +89,32 @@ function navHide() {
 
 // Box content animation
 
-var boxContent = document.getElementsByClassName('box-content');
-var parentBox = document.getElementsByClassName('learned');
-var countBox;
+var boxContent = document.getElementsByClassName('hover-content');
+var closeBtn = document.getElementsByClassName('close-btn');
 
-// console.log(boxContent);
-// console.log(parentBox);
+for (var i = 0; i < boxContent.length ; i++) {
+    boxContent[i].addEventListener('mouseenter',expandBox);
+}
 
-boxContent[0].addEventListener('mouseenter',expandBox);
+closeBtn[0].addEventListener('click', closeBox);
 
 function expandBox() {
-    var current = this.parentNode;
-    var parentCurrent = this.parentNode.parentNode;
+    var currentContent = this.nextElementSibling;
+    var current = this.parentNode.parentNode;
+    var parentCurrent = current.parentNode;
     var node = parentCurrent.childNodes;
     var currentChild = current.childNodes;
 
-    for (var i = 0; i < currentChild.length ; i++) {
-        if (currentChild[i].nodeType === 1) {
-            currentChild = currentChild[i];
+    for (var j = 0; j < currentChild.length ; j++) {
+        if (currentChild[j].nodeType === 1) {
+            currentChild = currentChild[j];
         }
     }
 
-    for (var i = 0; i < node.length ; i++) {
-        if (node[i].nodeType === 1) {
-            if (current !== node[i]) {
-                TweenMax.to(node[i], 0.3, {
+    for (var k = 0; k < node.length ; k++) {
+        if (node[k].nodeType === 1) {
+            if (current !== node[k]) {
+                TweenMax.to(node[k], 0.3, {
                     delay: 0.2,
                     width: '0',
                     opacity: 0,
@@ -121,19 +122,38 @@ function expandBox() {
                         TweenMax.to(current, 0.4, {
                             delay: 0.2,
                             className: '+=fullWidth',
-                            conComplete: function() {
+                            onComplete:function() {
                                 TweenMax.to(current, 0.2, {
-                                    delay: 0.2,
+                                    delay: 0.1,
                                     className: '+=fullHeight',
                                     onComplete: function() {
                                         TweenMax.to(currentChild, 0.2, {
                                             delay: 0.2,
                                             className: '+=hideShadow',
                                             onComplete: function() {
-                                                TweenMax.to(parentCurrent, 0.2, {
-                                                    delay: 0.2,
-                                                    className: '+=totalHeight',
-
+                                                TweenMax.to(boxContent, 0.2, {
+                                                    autoAlpha: 0,
+                                                    display:'hidden',
+                                                    onComplete: function() {
+                                                        TweenMax.to(parentCurrent, 0.2, {
+                                                            delay: 0.2,
+                                                            className: '+=totalHeight',
+                                                            onComplete: function() {
+                                                                TweenMax.to(currentContent, 0.2, {
+                                                                    autoAlpha: 1,
+                                                                    display: 'block',
+                                                                    onComplete: function() {
+                                                                        TweenMax.to(closeBtn, 0.2, {
+                                                                            delay: 0.2,
+                                                                            width: '50px',
+                                                                            right: '0',
+                                                                            opacity: 1,
+                                                                        })
+                                                                    }
+                                                                })
+                                                            }
+                                                        })
+                                                    }
                                                 })
                                             }
                                         })
@@ -149,8 +169,116 @@ function expandBox() {
 }
 
 function closeBox() {
-    // Click close button
-    // height back to normal
-    // Width back to normal
-    // Reapear the hiddens boxes
+
+    // Find openened element
+    // Remove content
+    // Remove extra classes from kids
+
+    var openenedParentElement = document.getElementsByClassName('totalHeight'),
+    openenedChildElementHeight = document.getElementsByClassName('fullHeight'),
+    openenedChildElementWidth = document.getElementsByClassName('fullWidth'),
+    openedGrandChild = openenedChildElementHeight[0].childNodes;
+    var openedGrandGrandChild;
+
+    for (var i = 0; i < openedGrandChild.length ; i++) {
+        if (openedGrandChild[i].nodeType === 1 ) {
+            openedGrandGrandChild = openedGrandChild[i];
+        }
+    }
+
+    var hoverContent = document.querySelector('.hover-content'),
+        overlayContent = openedGrandGrandChild.querySelector('.overlay-content');
+
+        console.log(hoverContent);
+
+
+    TweenMax.to(overlayContent, 0.2, {
+        autoAlpha: 0,
+        display: 'none',
+        onComplete: function() {
+            TweenMax.to(closeBtn, 0.2, {
+                delay: 0.4,
+                right: '0',
+                opacity: 0,
+                width: '0',
+                onComplete: function() {
+                    TweenMax.to(openenedParentElement[0], 0.2, {
+                        delay: 0.2,
+                        onComplete: function() {
+                            openenedParentElement[0].classList.remove('totalHeight');
+                            openenedChildElementHeight[0].classList.remove('fullHeight');
+                            openenedChildElementWidth[0].classList.remove('fullWidth');
+                            var boxShadow = document.getElementsByClassName('box-content');
+                            console.log(boxShadow);
+
+                            for (var k = 0; k < boxShadow.length ; k++) {
+                                boxShadow[k].classList.remove('hideShadow');
+                            }
+                            var displayBoxes = document.getElementsByClassName('box');
+                            for (var j = 0; j < displayBoxes.length ; j++) {
+                                displayBoxes[j].style.opacity = 1;
+                                displayBoxes[j].style.width = '100%';
+                            }
+
+                            TweenMax.to(hoverContent, 0.2, {
+                                delay: 0.2,
+                                autoAlpha: 1,
+                                display:'block',
+                            })
+                        }
+                    })
+                }
+            })
+        }
+    })
+}
+
+// Skills show
+// Display from bottom with a height of 5vh the box
+// Find child span
+// Display it
+
+var skillSelected = document.getElementsByClassName('skill-selected');
+var skillList = [];
+var skillBox = document.getElementById('skill-box');
+var closeSkills = document.getElementById('close-slide');
+
+for (var i = 0; i < skillSelected.length ; i++) {
+    skillSelected[i].addEventListener('click', showSkills);
+}
+
+closeSkills.addEventListener('click', hideSkills);
+
+function showSkills(e) {
+    var skillShow = e.target.querySelector('span').innerHTML;
+    TweenMax.to(skillBox, 0.2, {
+        delay: 0.3,
+        height: '35vh',
+        onComplete: function(e) {
+            TweenMax.to(closeSkills, 0.2, {
+                delay: 0.2,
+                width: '30px',
+                right: '0',
+                opacity: 1,
+                onComplete: function() {
+                    skillBox.querySelector('p').innerHTML = skillShow;
+                }
+            })
+        }
+    })
+}
+function hideSkills() {
+    TweenMax.to(closeSkills, 0.2, {
+        delay: 0.2,
+        width: '0',
+        right: '0',
+        opacity: 0,
+        onComplete: function() {
+            skillBox.querySelector('p').innerHTML = '';
+            TweenMax.to(skillBox, 0.2, {
+                delay: 0.3,
+                height: '0',
+            })
+        }
+    })
 }
